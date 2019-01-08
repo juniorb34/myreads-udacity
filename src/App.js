383b9c6740f636d2aa4bb744739f67361a636a6e
João Bosco <juniorb34fortal@gmail.com>
2019-01-08 19:16:00 -0300

    feat: construct components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,57 @@
-import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
-import * as BooksAPI from './Utils/BooksAPI'
-import ListBook from './Components/List_Book'
-import BookSearch from './Components/Book_Search'
-import './App.css'
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import * as BooksAPI from "./Utils/BooksAPI";
+import ListBook from "./components/Book_List";
+import BookSearch from "./components/Book_Search";
+import "./App.css";
 
 class BooksApp extends Component {
   state = {
     Books: []
-  }
+  };
 
   componentDidMount() {
-    this.fetch_books_details()
+    this.fetch_books_details();
   }
 
   fetch_books_details = () => {
-    BooksAPI.getAll().then((books) => {
-      this.setState({Books: books})
-    })
-  }
+    BooksAPI.getAll().then(books => {
+      this.setState({ Books: books });
+    });
+  };
 
   update_books_details = (book, shelf) => {
     BooksAPI.update(book, shelf).then(() => {
-      this.fetch_books_details()
-    })
-  }
+      this.fetch_books_details();
+    });
+  };
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" render={() => (<ListBook books={this.state.Books} onChange={this.update_books_details}/>)}/>
-        <Route exact path="/search" render={({history}) => (<BookSearch onChange={this.update_books_details} myBooks={this.state.Books}/>)}/>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <ListBook
+              books={this.state.Books}
+              onChange={this.update_books_details}
+            />
+          )}
+        />
+        <Route
+          exact
+          path="/search"
+          render={({ history }) => (
+            <BookSearch
+              onChange={this.update_books_details}
+              myBooks={this.state.Books}
+            />
+          )}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default BooksApp
+export default BooksApp;
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,44 +1,46 @@
 import React, { Component } from "react";
+import { PropTypes } from "prop-types";
 
 class Book extends Component {
-  render() {
-    const { book, onUpdateBooks } = this.props;
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onUpdate: PropTypes.func.isRequired
+  };
 
+  change_bookShelf = e => {
+    this.props.onUpdate(e.target.value);
+  };
+
+  render() {
+    const book = this.props.book;
     return (
-      <div className="book">
-        <div className="book-top">
-          {book.imageLinks ? (
+      <li>
+        <div className="book">
+          <div className="book-top">
             <div
               className="book-cover"
               style={{
                 width: 128,
                 height: 193,
-                backgroundImage: `url(${book.imageLinks.thumbnail})`
+                backgroundImage: `url("${book.imageLinks.thumbnail}")`
               }}
             />
-          ) : (
-            <div className="book-cover" style={{ width: 128, height: 193 }}>
-              <h4>Sem Imagem</h4>
+            <div className="book-shelf-changer">
+              <select onChange={this.change_bookShelf} value={book.shelf}>
+                <option value="none" disabled>
+                  Move to...
+                </option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
             </div>
-          )}
-          <div className="book-shelf-changer">
-            <select
-              value={book.shelf ? book.shelf : "none"}
-              onChange={event => onUpdateBooks(book, event.target.value)}
-            >
-              <option value="" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
           </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">{book.authors}</div>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.author}</div>
-      </div>
+      </li>
     );
   }
 }
